9d45de869ae9f66487967bcca190a019550cf441
khobot <vladislav.khobot@gmail.com>
2021-09-08 08:43:30 +0300

    feat(socket-man): Update hint
    - edit server/public
    
    Closes hint-update

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ab7a808f0ec8657fd42bd6f0f6f2ced5.js"
+  "/precache-manifest.88e11edda508651ef8cba8f8ea81efb0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
